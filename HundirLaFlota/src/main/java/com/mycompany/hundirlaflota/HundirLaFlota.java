9c861ed5a7dca412595d5adcd00fc05e6df45e68
M. <cuentamaya@gmail.com>
2025-12-17 16:03:10 +0100

    Esqueleto de hundir la flota
    main y clase barco vacias

---
+++ HundirLaFlota/src/main/java/com/mycompany/hundirlaflota/HundirLaFlota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hundirlaflota;
+
+/**
+ *
+ * @author DAM112
+ */
+public class HundirLaFlota {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
